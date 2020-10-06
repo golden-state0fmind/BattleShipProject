3e38fc37f9a85a07047556347dac6bfc959ea2b5
golden-state0fmind <areyes2009@yahoo.com>
2020-10-05 18:25:34 -0700

    added two D array

--- app.js
+++ app.js
@@ -7,19 +7,23 @@ let battleShip = {
     board: [],
     // generate the ships with values and classes
     getSpaces: function () {
-        for (i = 0; i < 63; i++) {
-            let space = document.createElement('div')
-            space.setAttribute('class', 'holdSpace')
-            space.setAttribute('value', 1)
-            grid.appendChild(space)
-            space.addEventListener('click', this.fireMissle)
-            this.board.push(space)
+        for (i = 0; i < 9; i++) {
+            let temp = []
+            for (j = 0; j < 9; j++) {
+                let space = document.createElement('div')
+                space.setAttribute('class', 'holdSpace')
+                space.setAttribute('value', `${i}-${j}`)
+                grid.appendChild(space)
+                space.addEventListener('click', this.fireMissle)
+                temp.push(space)
+            }
+            this.board.push(temp)
         }
     },
     // select and listen to the space and check to see if the space is a ship or empty 
     fireMissle: function (e) {
-        if (e.target.className === 'holdSpace' && e.target.id === 'ship') {
-            e.target.style.opacity = 0
+        if (e.target.className === 'holdSpace' && e.target.style.backgroundColor === 'red') {
+            e.target.style.opacity = 0.3
             alert('Successful')
         } else {
             alert('Missed!!')
@@ -27,14 +31,16 @@ let battleShip = {
         }
     },
     // add image or access the value of the ship to compare true(hit) or false(missed) create ships and place in array then try to hit that space to return true
-    buildShips: function () {
-        console.log(battleShip.board)
-        console.log(battleShip.board[0], [1], [2])
-    }
+    // buildShips: function () {
+    //     battleShip.board[0].style.backgroundColor = 'red'
+    //     battleShip.board[1].style.backgroundColor = 'red'
+    //     battleShip.board[2].style.backgroundColor = 'red'
+    //     console.log(battleShip.board[0], [1], [2])
+    // }
 
     //randomize the ships
 
 }
 battleShip.getSpaces()
-battleShip.buildShips()
-
+// battleShip.buildShips()
+console.log(battleShip.board)
