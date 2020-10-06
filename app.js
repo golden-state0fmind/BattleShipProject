3951efbb3871c3e8502b3f85cc0b365aa7d7172d
golden-state0fmind <areyes2009@yahoo.com>
2020-10-05 22:50:46 -0700

    working on event listener for input field

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 console.log('sanity check')
 // create a space to hold the ships 
 let grid = document.querySelector('.container')
+// adding an input field to register fire missle coordinates
+let form = document.querySelector('form')
+let inputGuess = document.querySelector('input')
+console.log(inputGuess)
+
 
 let battleShip = {
     // store the ships in an Array
@@ -22,25 +27,28 @@ let battleShip = {
     },
     // select and listen to the space and check to see if the space is a ship or empty 
     fireMissle: function (e) {
-        if (e.target.className === 'holdSpace' && e.target.style.backgroundColor === 'red') {
+        if (e.target.value === form.value) {
             e.target.style.opacity = 0.3
             alert('Successful')
         } else {
             alert('Missed!!')
             e.target.style.opacity = .2
         }
+        e.preventDefault() // prevents the form from leaving the page
     },
     // add image or access the value of the ship to compare true(hit) or false(missed) create ships and place in array then try to hit that space to return true
-    // buildShips: function () {
-    //     battleShip.board[0].style.backgroundColor = 'red'
-    //     battleShip.board[1].style.backgroundColor = 'red'
-    //     battleShip.board[2].style.backgroundColor = 'red'
-    //     console.log(battleShip.board[0], [1], [2])
-    // }
+    buildShips: function () {
+        battleShip.board[0][0].setAttribute('class', 'ship')
+        battleShip.board[0][1].setAttribute('class', 'ship')
+        battleShip.board[0][2].setAttribute('class', 'ship')
+        //console.log(battleShip.board[0][0], [0][1], [0][2])
+    },
 
-    //randomize the ships
 
 }
+// listening for an input to compare coordinates on our grid
+form.addEventListener('submit', battleShip.fireMissle)
 battleShip.getSpaces()
-// battleShip.buildShips()
-console.log(battleShip.board)
+battleShip.buildShips()
+//console.log(battleShip.board[0][0])
+console.log(inputGuess)
