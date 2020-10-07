6d2e65a0a054543eb40f53a0d51018bae2c6214b
golden-state0fmind <areyes2009@yahoo.com>
2020-10-07 14:35:05 -0700

    added cpu guess will need to randomize

--- app.js
+++ app.js
@@ -41,7 +41,16 @@ let battleShip = {
             this.playerGrid.push(player)
         }
     },
-
+    cpuMissle: function () {
+        console.log('i am random cpu guess')
+        cpuGuess = battleShip.playerGrid[4][0]
+        console.log(cpuGuess)
+        if (cpuGuess) {
+            battleShip.playerGrid[4][0].setAttribute('class', 'ocean')
+            alert('Cpu Missed!! Players Turn')
+        }
+        console.log('almost')
+    },
     // select and listen to the space and check to see if the space is a ship or empty 
     fireMissle: function (e) {
         e.preventDefault()// prevents the form from leaving the page
@@ -94,34 +103,36 @@ let battleShip = {
             alert('Missed Target!!')
             document.getElementById(e.target[0].value).setAttribute('class', 'ocean')
             inputGuess.value = ''
+            battleShip.cpuMissle()
         }
     },
     // add image or access the value of the ship to compare true(hit) or false(missed) create ships and place in array then try to hit that space to return true
     cpuShips: function () {
         //first ship on the CpuBoard near the top left
-        battleShip.cpuGrid[0][0].setAttribute('class', 'ship')
-        battleShip.cpuGrid[0][1].setAttribute('class', 'ship')
-        battleShip.cpuGrid[0][2].setAttribute('class', 'ship')
+        battleShip.cpuGrid[0][0].setAttribute('class', 'cpuShip')
+        battleShip.cpuGrid[0][1].setAttribute('class', 'cpuShip')
+        battleShip.cpuGrid[0][2].setAttribute('class', 'cpuShip')
         //second ship along the bottom right edge
-        battleShip.cpuGrid[4][8].setAttribute('class', 'ship')
-        battleShip.cpuGrid[5][8].setAttribute('class', 'ship')
-        battleShip.cpuGrid[6][8].setAttribute('class', 'ship')
-        battleShip.cpuGrid[7][8].setAttribute('class', 'ship')
-        battleShip.cpuGrid[8][8].setAttribute('class', 'ship')
+        battleShip.cpuGrid[4][8].setAttribute('class', 'cpuShip')
+        battleShip.cpuGrid[5][8].setAttribute('class', 'cpuShip')
+        battleShip.cpuGrid[6][8].setAttribute('class', 'cpuShip')
+        battleShip.cpuGrid[7][8].setAttribute('class', 'cpuShip')
+        battleShip.cpuGrid[8][8].setAttribute('class', 'cpuShip')
         //console.log(battleShip.board[0][0], [0][1], [0][2])
     },
     playerShips: function () {
         //first ship on the playerBoard 
-        battleShip.playerGrid[2][5].setAttribute('class', 'ship')
-        battleShip.playerGrid[2][6].setAttribute('class', 'ship')
-        battleShip.playerGrid[2][7].setAttribute('class', 'ship')
+        battleShip.playerGrid[2][5].setAttribute('class', 'playerShip')
+        battleShip.playerGrid[2][6].setAttribute('class', 'playerShip')
+        battleShip.playerGrid[2][7].setAttribute('class', 'playerShip')
         //second ship on the PlayerBoard
-        battleShip.playerGrid[4][1].setAttribute('class', 'ship')
-        battleShip.playerGrid[5][1].setAttribute('class', 'ship')
-        battleShip.playerGrid[6][1].setAttribute('class', 'ship')
-        battleShip.playerGrid[7][1].setAttribute('class', 'ship')
-        battleShip.playerGrid[8][1].setAttribute('class', 'ship')
-    }
+        battleShip.playerGrid[4][1].setAttribute('class', 'playerShip')
+        battleShip.playerGrid[5][1].setAttribute('class', 'playerShip')
+        battleShip.playerGrid[6][1].setAttribute('class', 'playerShip')
+        battleShip.playerGrid[7][1].setAttribute('class', 'playerShip')
+        battleShip.playerGrid[8][1].setAttribute('class', 'playerShip')
+    },
+
 }
 // listening for an input to compare coordinates on our grid
 form.addEventListener('submit', battleShip.fireMissle)
